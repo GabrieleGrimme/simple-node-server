329bfe0f3bd3053cf7784eec2b30132c69a8015f
Gabriele Grimme <gabriele-grimme@web.de>
2021-05-27 13:05:30 +0200

    einige updates des codes

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -5,14 +5,10 @@ import { loadFromDatabase, saveToDatabase } from './lib/databaseHelpers.js';
 
 function postProduct(request, response) {
     const newProduct = { ...request.body, id: uuidv4() };
-
     const database = loadFromDatabase();
     const products = database.products;
-
     products.push(newProduct);
-
     saveToDatabase(database);
-
     response.json(newProduct);
 }
 
@@ -23,17 +19,14 @@ function getProducts(request, response) {
 
 function getProduct() {
     const { productId } = request.params;
-
     const database = loadFromDatabase();
     const products = database.products;
-
     const product = products.find((product) => product.id === productId);
-
     if (product) {
         response.json(product);
     } else {
         response.json({ message: 'Product with id ' + productId + ' not found.'});
     }
-}
+};
 
 export { getProduct, getProducts, postProduct };
